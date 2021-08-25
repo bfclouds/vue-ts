7cf460068f0edf0de8696d749378d40578f488c9
wenjiangf <f_wenjiang@163.com>
2021-08-25 15:04:53 +0800

    添加启动页文字

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>项目启动起来了，开始你的表演吧，骚年！！</div>
-  <div>{{ baseStore.count }}</div>
-  <div>{{ baseStore.doubleCount }}</div>
+  <h1 class="text">项目启动起来了，开始你的表演吧，骚年！！</h1>
+  <!-- <div>{{ baseStore.count }}</div>
+  <div>{{ baseStore.doubleCount }}</div> -->
 </template>
 
 <script lang="ts" setup>
@@ -9,3 +9,24 @@ import { useBaseStore } from '/@/store/modules/moduleA'
 
 const baseStore = useBaseStore()
 </script>
+
+<style scoped>
+  .text {
+    background-image: linear-gradient(90deg, #f66, #f90);
+    -webkit-background-clip: text;
+    background-clip: text;
+    line-height: 60px;
+    font-size: 30px;
+    color: transparent;
+    animation: hue 5s linear infinite;
+    text-align: center;
+  }
+  @keyframes hue {
+    from {
+      filter: hue-rotate(0);
+    }
+    to {
+      filter: hue-rotate(-1turn);
+    }
+  }
+</style>
